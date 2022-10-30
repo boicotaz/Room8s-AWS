6fab53bfa132b89f447424a2003e2be0b6bb098e
boicotaz <tolisgerodimos94@gmail.com>
2022-10-30 14:08:36 +0200

    Change(VPC): Removed private subnets.

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ module "vpc" {
   cidr                          = "10.0.0.0/16"
   create_igw                    = true
   public_subnets                = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets               = ["10.0.3.0/24", "10.0.4.0/24"]
   manage_default_security_group = true
   default_security_group_ingress = [
     {

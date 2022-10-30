78dd9a939baaed1090f206cf0f45210dee769d2f
boicotaz <tolisgerodimos94@gmail.com>
2022-10-30 14:03:24 +0200

    Refactor(SG): Removed development security group rules, refactored file

--- sg.tf
+++ sg.tf
@@ -1,58 +1,51 @@
 # ALB Security Group (Traffic Internet -> ALB)
-resource "aws_security_group" "load-balancer" {
-  name        = "load_balancer_sg"
-  description = "Allow Access to 80 and 443 of LB"
+resource "aws_security_group" "load_balancer" {
+
+  name        = "lb-sg"
+  description = "Allow access to lb to port 80"
   vpc_id      = module.vpc.vpc_id
 
+
   ingress {
-    description = "HTTPS from ME"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["87.202.119.136/32"]
-  }
-  ingress {
-    description = "HTTP from ME"
+    description = "HTTP from anywhere"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["87.202.119.136/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
 }
+
 # ECS Security group (traffic ALB -> ECS, ssh -> ECS)
-resource "aws_security_group" "ecs" {
-  name        = "ecs_sg"
-  description = "Allow Traffic from ALB"
+resource "aws_security_group" "ecs_service" {
+
+  name        = "HTTP"
+  description = "Allow all HTTP inbound traffic"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    description     = "Allow access from ALB"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = [aws_security_group.load-balancer.id]
-  }
-  ingress {
-    description = "ssh from ME"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["87.202.119.136/32"]
+    description     = "HTTP from anywhere"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load_balancer.id]
+    #cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
 }

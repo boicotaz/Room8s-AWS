862f2e6d86609896c23f4401d8672aa0c5b9e972
boicotaz <tolisgerodimos94@gmail.com>
2022-11-05 17:06:05 +0200

    Add (ECS Service): Added ECS Service Role

--- iam.tf
+++ iam.tf
@@ -2,13 +2,13 @@
 # Used for access to ECR and Cloudwatch mainly
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "${var.ecs_cluster_name}-execution-task-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.assume_ecs_task_role_policy.json
   tags = {
-    Name = "${var.ecs_cluster_name}-iam-role"
+    Name = "${var.ecs_cluster_name}-ecs-task-iam-role"
   }
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
+data "aws_iam_policy_document" "assume_ecs_task_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -24,3 +24,29 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 ###########################################
+
+# Create custom ServiceRoleForECS
+resource "aws_iam_role" "ServiceRoleForECS" {
+  name               = "${var.ecs_cluster_name}-ecs-service-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_ecs_service_role_policy.json
+  tags = {
+    Name = "${var.ecs_cluster_name}-ecs-service-iam-role"
+  }
+}
+
+data "aws_iam_policy_document" "assume_ecs_task_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "ecs-service-role-policy" {
+  name   = "ecs_service_role_policy"
+  policy = file("policies/ecs-service-role-policy.json")
+  role   = aws_iam_role.ServiceRoleForECS.id
+}

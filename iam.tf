d69753a98de063248a62b4d7b71cd71b37486070
boicotaz <tolisgerodimos94@gmail.com>
2022-11-06 14:03:08 +0200

    Add (Container Instances): Added container instances ASG and roles for use in ECS Cluster

--- iam.tf
+++ iam.tf
@@ -23,35 +23,6 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-###########################################
-
-# Create custom ServiceRoleForECS role
-resource "aws_iam_role" "ServiceRoleForECS" {
-  name               = "${var.ecs_cluster_name}-ecs-service-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_ecs_service_role_policy.json
-  tags = {
-    Name = "${var.ecs_cluster_name}-ecs-service-iam-role"
-  }
-}
-
-data "aws_iam_policy_document" "assume_ecs_task_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy" "ecs-service-role-policy" {
-  name   = "ecs_service_role_policy"
-  policy = file("policies/ecs-service-role-policy.json")
-  role   = aws_iam_role.ServiceRoleForECS.id
-}
-
-###########################################
 
 # Create custom ecsInstanceProfile
 
@@ -77,8 +48,9 @@ data "aws_iam_policy_document" "assume-ecs-instance-role-policy" {
   }
 }
 
-resource "aws_iam_role_policy" "ecs-service-role-policy" {
+resource "aws_iam_role_policy" "ecs-instance-role-policy" {
   name   = "ecs_instance_role_policy"
   policy = file("policies/ecs-instance-role-policy.json")
-  role   = aws_iam_role.ecs-instance-role.id
+  #policy = file("policies/ecs-role.json")
+  role = aws_iam_role.ecs-instance-role.id
 }
